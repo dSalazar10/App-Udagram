539b9fa8f2c588c5361868fe0f0f545dd819057e
djs <danieljsalazar2@icloud.com>
2019-07-02 23:41:31 -0700

    New feature working on dev servers. Postman tests were successful. Moving to stage.

--- Image_Filter_Server/src/server.ts
+++ Image_Filter_Server/src/server.ts
@@ -12,32 +12,31 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
   const port = process.env.PORT || 8082;
 
   // Validate URL
+  // Does it contain an image file type?
   const isImageUrl = require('is-image-url');
+
+  // Verify URL
+  // Does it exist?
+  const urlExists = require('url-exists-deep');
 
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
   
-  // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req: Request, res:Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
 
-  // endpoint to filter an image from a public url
+  // Filter an image based on a public url
   app.get( "/filteredimage", async ( req: Request, res:Response ) => {
     const email = req.body.email;
     const password = req.body.password;
-    // check email is valid
-    if (!email || !EmailValidator.validate(email)) {
-      return res.status(400).send({ auth: false, message: `Failed to authenticate.` });
-    }
-
-    // check email password valid
-    if (!password) {
+    // Verify email, verify password, validate email
+    if (!email || !password || !EmailValidator.validate(email) ) {
       return res.status(400).send({ auth: false, message: `Failed to authenticate.` });
     }
     // VERY BAD - storing a single user/pass in proc env and compare in memory
-    // A cheap way to block public access
+    // A quick and easy way to block public access
     // @TODO: upgrade security
     if (email !== process.env.ISERVER_USER) {
       return res.status(400).send({ auth: false, message: `Failed to authenticate.` });
@@ -45,38 +44,39 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
     if (password !== process.env.ISERVER_PASSWORD) {
       return res.status(400).send({ auth: false, message: `Failed to authenticate.` });
     }
-
     // URL of a publicly accessible image
-    const { image_url } = req.query.image_url;
-
-    // validate the image_url query
+    const { image_url } = req.query;
+    // Verify query and validate url
     if ( !image_url || !isImageUrl(image_url) ) {
       res.status(400).send(`image_url required`);
     }
-
-    // Filter the image
-    const filteredImage = await filterImageFromURL(image_url).catch( (err) => {
-      if (err) {
+    // Validate url
+    urlExists(image_url).then(function(exists: { href: any; }){
+      if (!exists) {
         return res.status(400).send(`bad image_url`);
-      }
-    });
-    const data: string = filteredImage as string;
-    // Send the resulting file in the response
-    res.sendFile(data, {}, function (err) {
-      if (err) {
-        throw err;
       } else {
-        // Deletes any files on the server on finish of the response
-        deleteLocalFiles([data]).catch( (err) => {
+        // Filter the image
+        filterImageFromURL(image_url).then( (data) => {
+          // Send the resulting file in the response
+          res.sendFile(data, {}, function (err) {
+            if (err) {
+              throw err;
+            } else {
+              // Deletes any files on the server on finish of the response
+              deleteLocalFiles([data]).catch( (err) => {
+                if (err) {
+                  return res.status(400).send(`bad image_url`);
+                }
+              });
+            }
+          });
+        }).catch( (err) => {
           if (err) {
-            return res.status(400).send(`bad image_url`);
+            throw err;
           }
-        });
+        })
       }
-
-      // End sendFile
     });
-
   });
 
   // Start the Server
