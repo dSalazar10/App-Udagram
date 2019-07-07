67654c01b52c74fd2abf221589e4261e4e4ea3b4
djs <danieljsalazar2@icloud.com>
2019-07-07 06:47:08 -0700

    fix(Fix Image Filter Server): Fixed a bug that prevented S3 access
    Added a wrapper function that creates new credentials when NOT deployed.

--- RestAPI_Server/src/aws.ts
+++ RestAPI_Server/src/aws.ts
@@ -4,8 +4,12 @@ import { config } from './config/config';
 const c = config.dev;
 
 // Configure AWS
-const credentials = new AWS.SharedIniFileCredentials({profile: c.aws_profile});
-AWS.config.credentials = credentials;
+if (c.aws_profile !== 'DEPLOYED') {
+    console.log('\n\n\n\nThis shouldn\'t happen in deployed state\n\n\n\n')
+    const credentials = new AWS.SharedIniFileCredentials({profile: c.aws_profile});
+    AWS.config.credentials = credentials;
+}
+
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
