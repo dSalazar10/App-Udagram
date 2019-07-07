5c64acd52516ca2cf7aea684b7958e1e6f9e24a1
djs <danieljsalazar2@icloud.com>
2019-07-07 09:26:55 -0700

    style(Update Image Filter Server): Reorganize Image Filter
    Reorganized the code to be more flexible.
    Code cleanup.

--- Image_Filter_Server/src/controllers/v0/filter/routes/filter.router.ts
+++ Image_Filter_Server/src/controllers/v0/filter/routes/filter.router.ts
@@ -12,6 +12,83 @@ const isImageUrl = require('is-image-url');
 // Does it exist?
 const urlExists = require('url-exists-deep');
 
+async function greyFilter(photo: Jimp) {
+    photo.resize(256, 256)
+        .quality(60)
+        .greyscale();
+    return photo;
+}
+async function sepiaFilter(photo: Jimp) {
+    photo.resize(256, 256)
+        .quality(60)
+        .sepia();
+    return photo;
+}
+async function blurFilter(photo: Jimp) {
+    photo.resize(256, 256)
+        .quality(60)
+        .blur(7);
+    return photo;
+}
+async function gaussianFilter(photo: Jimp) {
+    photo.resize(256, 256)
+        .quality(60)
+        .gaussian(7);
+    return photo;
+}
+async function mirrorFilter(photo: Jimp) {
+    photo.resize(256, 256)
+        .quality(60)
+        .mirror(true, false);
+    return photo;
+}
+async function invertFilter(photo: Jimp) {
+    photo.resize(256, 256)
+        .quality(60)
+        .invert();
+    return photo;
+}
+
+// filterImage
+// Validates the URL, filters the image, sends it back, and deletes local files
+// INPUTS
+//     image_url: string - the url of a publicly accessible image
+//     res: Response - the calling function's response variable
+//     type: number - the filter requested
+// RETURNS
+//     nothing
+async function filterImage(image_url: string, res: Response, type: number) {
+    // Validate url
+    urlExists(image_url).then(function(exists: { href: any; }) {
+        if (!exists) {
+            return res.status(400).send(`${image_url} does not exist.`);
+        } else {
+            // Filter the image
+            filterImageFromURL(image_url, type).then( (data) => {
+                // Send the resulting filtered image to caller
+                res.sendFile(data, {}, function (send_err: any) {
+                    if (!send_err) {
+                        // Deletes any files on the server on finish of the response
+                        deleteLocalFiles([data]).catch( (del_err: any) => {
+                            if (!del_err) {
+                                return res.status(400).send(`bad image_url`);
+                            } else {
+                                console.log(del_err);
+                            }
+                        }).catch( (del_throw) => {
+                            console.log(del_throw);
+                        });
+                    } else {
+                        console.log(send_err);
+                    }
+                });
+            }).catch( (send_throw) => {
+                console.log(send_throw);
+            });
+        }
+    });
+}
+
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
 // returns the absolute path to the local image
@@ -21,57 +98,50 @@ const urlExists = require('url-exists-deep');
 //    an absolute path to a filtered image locally saved file
 async function filterImageFromURL(inputURL: string, type: number): Promise<string> {
     return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
+        const photo: Jimp = await Jimp.read(inputURL);
         const outPath = '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
         switch (type) {
-            case 1: await photo
-                .resize(256, 256) // resize
-                .quality(60) // set JPEG quality
-                .greyscale() // set greyscale
-                .write(__dirname + outPath, () => {
-                resolve(__dirname + outPath);
-            });
-            break;
-            case 2: await photo
-                .sepia()
-                .resize(256, 256) // resize
-                .quality(60) // set JPEG quality
-                .write(__dirname + outPath, () => {
-                resolve(__dirname + outPath);
-            });
-            break;
-            case 3: await photo
-                .resize(256, 256) // resize
-                .quality(60) // set JPEG quality
-                .blur(7)
-                .write(__dirname + outPath, () => {
-                resolve(__dirname + outPath);
-            });
-            break;
-            case 4: await photo
-                .resize(256, 256) // resize
-                .quality(60) // set JPEG quality
-                .gaussian(7)
-                .write(__dirname + outPath, () => {
-                resolve(__dirname + outPath);
-            });
-            break;
-            case 5: await photo
-                .resize(256, 256) // resize
-                .quality(60) // set JPEG quality
-                .mirror(true, false)
-                .write(__dirname + outPath, () => {
-                resolve(__dirname + outPath);
-            });
-            break;
-            default: await photo
-                .resize(256, 256) // resize
-                .quality(60) // set JPEG quality
-                .mirror(true, false)
-                .sepia()
-                .write(__dirname + outPath, () => {
-                resolve(__dirname + outPath);
-            });
+            case 1:
+                await greyFilter(photo).then( (img) => {
+                    img.write(__dirname + outPath, () => {
+                        resolve(__dirname + outPath);
+                    });
+                });
+                break;
+            case 2:
+                await sepiaFilter(photo).then( (img) => {
+                    img.write(__dirname + outPath, () => {
+                        resolve(__dirname + outPath);
+                    });
+                });
+                break;
+            case 3:
+                await blurFilter(photo).then( (img) => {
+                    img.write(__dirname + outPath, () => {
+                        resolve(__dirname + outPath);
+                    });
+                });
+                break;
+            case 4:
+                await gaussianFilter(photo).then( (img) => {
+                    img.write(__dirname + outPath, () => {
+                        resolve(__dirname + outPath);
+                    });
+                });
+                break;
+            case 5:
+                await mirrorFilter(photo).then( (img) => {
+                    img.write(__dirname + outPath, () => {
+                        resolve(__dirname + outPath);
+                    });
+                });
+                break;
+            default:
+                await invertFilter(photo).then( (img) => {
+                    img.write(__dirname + outPath, () => {
+                        resolve(__dirname + outPath);
+                    });
+                });
         }
     });
 }
@@ -94,42 +164,23 @@ async function deleteLocalFiles(files: Array<string>) {
 * 3) blur
 * 4) gaussian
 * 5) mirror
+* 6) invert
 * */
 router.get( '/', requireAuth, async ( req: Request, res: Response ) => {
     // URL of a publicly accessible image
-    const { type, image_url } = req.query;
+    const { image_url } = req.query;
     // Verify query and validate url
     if ( !image_url || !isImageUrl(image_url) ) {
         res.status(400).send(`image_url required`);
     }
-    // Validate url
-    urlExists(image_url).then(function(exists: { href: any; }) {
-        if (!exists) {
-            return res.status(400).send(`bad image_url`);
-        } else {
-            // Filter the image
-            filterImageFromURL(image_url, type).then( (data) => {
-                // Send the resulting file in the response
-                res.sendFile(data, {}, function (err: any) {
-                    if (err) {
-                        throw err;
-                    } else {
-                        // Deletes any files on the server on finish of the response
-                        deleteLocalFiles([data]).catch( (derr: any) => {
-                            if (derr) {
-                                return res.status(400).send(`bad image_url`);
-                            }
-                        });
-                    }
-                });
-            }).catch( (err) => {
-                if (err) {
-                    throw err;
-                }
-            });
-        }
-    });
+    filterImage(image_url, res)
+        .catch( (filter_throw) => {
+            console.log(filter_throw);
+        });
+
 });
+
+router.post('/grey', requireAuth, async ( req: Request, res: Response ) => {});
 
 
 export const FilterRouter: Router = router;
