ee374c2894f5b36555515e36bdf67596f69e5ff4
djs <danieljsalazar2@icloud.com>
2019-07-08 13:12:01 -0700

    style(Add License): I added a license comment at the top of every item

--- RestAPI_Server/src/server.ts
+++ RestAPI_Server/src/server.ts
@@ -1,37 +1,51 @@
+/* MIT License
+
+Copyright (c) 2019 Daniel Salazar
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 import express from 'express';
-import { sequelize } from './sequelize';
-
-import { IndexRouter } from './controllers/v0/index.router';
-
 import bodyParser from 'body-parser';
-
-import { V0MODELS } from './controllers/v0/model.index';
+import {sequelize} from './sequelize';
+import {IndexRouter} from './controllers/v0/index.router';
+import {V0MODELS} from './controllers/v0/model.index';
 
 (async () => {
   await sequelize.addModels(V0MODELS);
   await sequelize.sync();
-
   const app = express();
   const port = process.env.PORT || 8080; // default port to listen
-
   app.use(bodyParser.json());
-
-  // CORS Should be restricted
   app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    //res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, HEAD');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
   });
-
+  // Redirect to index router for /api/v0/
   app.use('/api/v0/', IndexRouter);
-
-  // Root URI call
   app.get( '/', async ( req, res ) => {
-    res.send( '/api/v0/' );
-  } );
-
-
+    res.sendFile('server_index.html', {root: __dirname });
+  });
+  app.get( '/api', async ( req, res ) => {
+    res.sendFile('server_index.html', {root: __dirname });
+  });
   // Start the Server
   app.listen( port, () => {
       console.log( `server running http://localhost:${ port }` );
