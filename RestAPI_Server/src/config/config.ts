7d720e785ce7532433958be413f887accfbb31f3
djs <danieljsalazar2@icloud.com>
2019-07-07 19:16:28 -0700

    feat(Added Production servers): Production RestAPI and Image Filter Servers deployed
    DB and FS resources have been allocated and all servers are healthy.

--- RestAPI_Server/src/config/config.ts
+++ RestAPI_Server/src/config/config.ts
@@ -9,15 +9,18 @@ export const config = {
     'aws_profile': process.env.AWS_PROFILE,
     'aws_media_bucket': process.env.AWS_MEDIA_BUCKET,
     'jwt_secret': process.env.JWT_SECRET,
-    'filter_host': process.env.FILTER_HOST,
-    'filter_username': process.env.FILTER_USERNAME,
-    'filter_password': process.env.FILTER_PASSWORD
+    'filter_host': process.env.FILTER_HOST
   },
   'prod': {
-    'username': '',
-    'password': '',
-    'database': 'udagram_prod',
-    'host': '',
-    'dialect': 'postgres'
+    'username': process.env.POSTGRESS_USERNAME,
+    'password': process.env.POSTGRESS_PASSWORD,
+    'database': process.env.POSTGRESS_DATABASE,
+    'host': process.env.POSTGRESS_HOST,
+    'dialect': 'postgres',
+    'aws_reigion': 'us-east-2',
+    'aws_profile': process.env.AWS_PROFILE,
+    'aws_media_bucket': process.env.AWS_MEDIA_BUCKET,
+    'jwt_secret': process.env.JWT_SECRET,
+    'filter_host': process.env.FILTER_HOST
   }
 };
